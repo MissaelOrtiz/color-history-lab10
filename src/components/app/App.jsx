41024229bb4e6819adaf6408ad9e4b155ce977eb
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-22 16:35:22 -0700

    added inital file structure, useRecord custom hook needs logic

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,15 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const { current, undo, redo, record } = useRecord('#FF0000');
+
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input type="color" value={current} onChange={({ target }) => record(target.value)} />
+      <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
+    </>
+  );
 }
