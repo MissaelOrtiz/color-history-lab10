4f3feb5e8d64aa24c098d43c94de379e172a7e03
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-22 18:23:07 -0700

    changed useRecords useEffect to account for inital values as well as new values

--- src/custom-hooks/useRecord.js
+++ src/custom-hooks/useRecord.js
@@ -1,20 +1,30 @@
+/* eslint-disable max-len */
 import { useEffect, useState } from 'react';
 
 export const useRecord = (initialColor = '#00FF00') => {
   const [current, setCurrent] = useState(initialColor);
   const [history, setHistory] = useState([]);
+  const [newColor, setNewColor] = useState();
 
-  const record = (newColor) => {
-    setCurrent(newColor);
+  const record = (brandNewColor) => {
+    setNewColor(brandNewColor);
   };
 
-  const undo = () => {};
+  const undo = () => {
+    const currentIndex = history.indexOf(current);
+    const targetIndex = currentIndex !== 0 ? currentIndex - 1 : 0;
+    setCurrent(history[targetIndex]);
+  };
 
   const redo = () => {};
 
   useEffect(() => {
-    setHistory((previousHistory) => [...previousHistory, current]);
-  }, [current]);
+    setHistory((previousHistory) => {
+      const currentHistory = previousHistory ? [...previousHistory] : [];
+      const currentIndex = !previousHistory ? 0 : Number(previousHistory.indexOf(current) + 1);
+      currentHistory.splice(currentIndex, newColor);
+    });
+  }, [newColor]);
 
   return { current, undo, redo, record };
 };
