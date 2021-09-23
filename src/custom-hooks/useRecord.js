65beab75d09b704c3efc6bf8765889feec52ffa3
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-23 11:47:09 -0700

    Better record function within useRecord hook

--- src/custom-hooks/useRecord.js
+++ src/custom-hooks/useRecord.js
@@ -1,17 +1,17 @@
 /* eslint-disable max-len */
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export const useRecord = (initialColor = '#00FF00') => {
   const [current, setCurrent] = useState(initialColor);
-  const [history, setHistory] = useState([]);
-  const [currentIndex, setIndex] = useState(-1);
+  const [history, setHistory] = useState([initialColor]);
+  const [currentIndex, setIndex] = useState(0);
 
   const record = (brandNewColor) => {
-    const previousHistory = history.slice();
+    const Index = currentIndex + 1;
+    history.splice(Index, 0, brandNewColor);
+    setIndex(Index);
     setCurrent(brandNewColor);
-    setIndex(prevIndex => prevIndex + 1);
-    previousHistory[currentIndex] = brandNewColor;
-    setHistory(previousHistory);
+    setHistory(history);
   };
 
   const undo = () => {
@@ -30,10 +30,6 @@ export const useRecord = (initialColor = '#00FF00') => {
       setIndex(prevIndex => prevIndex + 1);
     }
   };
-
-  useEffect(() => {
-    record(initialColor);
-  }, []);
 
   return { current, undo, redo, record };
 };
